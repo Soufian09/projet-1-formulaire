6b19bffe5273240cc077456484f0d5099b4d51be
Soufian09 <46717440+Soufian09@users.noreply.github.com>
2019-04-02 13:31:19 +0200

    files navbar

---
+++ assets/nav/_NAVBAR.php
@@ -0,0 +1,43 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/_">
+    <script src="main.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-dark navbar-expand-md justify-content-between"> 
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-nav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse dual-nav w-50 order-1 order-md-0">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link pl-0" href="acceuil.html"><i class="fa fa-fw fa-home"></i>Acceuil <span class="sr-only">Acceuil</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="produits.html"><i class="fa fa fw fa-code"></i>Produits</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="form.html"><i class="fa fa-fw fa-envelope"></i>Contact</a>
+                    </li>
+                </ul>
+            </div>
+                <a href="/" class="navbar-brand mx-auto d-block text-center order-0 order-md-1 w-25"><img src="/img/hackers-poulette-logo.png" class="logo"></a>
+                <div class="navbar-collapse collapse dual-nav w-50 order-2">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="https://twitter.com/raspberry_pi"><i class="fa fa-twitter"></i></a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://fr-fr.facebook.com/raspberrypi"><i class="fa fa-facebook"></i></a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://github.com/raspberrypi"><i class="fa fa-github"></i></a></li>
+                    </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
