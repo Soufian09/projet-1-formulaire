17df0f0410ed0b90a4b1ee8ade75d2f46eafd804
Maxim Berge <maxim.berge@gmail.com>
2019-04-03 16:47:28 +0200

    ajout envoie du mail

--- contact.php
+++ contact.php
@@ -1,8 +1,13 @@
 <?php
+/*
+    **********************
+	**   SANITISATION   **
+	**********************
+*/
 // On créé un tableau associatif en précisant les filtres qu'on veut pour chaque input
     $filters_options = array(
         'first_name'    => FILTER_SANITIZE_STRING,
-        'last_Name' 	=> FILTER_SANITIZE_STRING,
+        'last_name' 	=> FILTER_SANITIZE_STRING,
         'email'         => FILTER_VALIDATE_EMAIL,
         'country' 	    => FILTER_SANITIZE_STRING,
         'subject' 		=> FILTER_SANITIZE_STRING,
@@ -11,7 +16,7 @@
     // SI le formulaire est envoyé ET que tous les champs sont remplis, on filtre les données et on les stock.
     // Sinon, on signale à l'utilisateur qu'il doit remplir les champs
     if(isset($_POST['submit'])){
-        if(!empty($_POST['first_name']) AND !empty($_POST['last_Name']) AND !empty($_POST['email']) AND !empty($_POST['subject']) AND !empty($_POST['message']) AND !empty($_POST['country']) AND empty($_POST['honney_Pot'])){
+        if(!empty($_POST['first_name']) AND !empty($_POST['last_name']) AND !empty($_POST['email']) AND !empty($_POST['subject']) AND !empty($_POST['message']) AND !empty($_POST['country']) AND empty($_POST['honney_Pot'])){
             // On créé un tableau associatif dans lequel on stoque les valeurs une fois filtrée
             $filter_result = filter_input_array(INPUT_POST, $filters_options);
             // Affichage des données --- !!!!!!!!!!! DEV ONLY !!!!!!!!!!! ---
@@ -23,7 +28,25 @@
             echo "Merci de remplir tous les champs!";
         }
     }
+
+/*
+    *************************
+	**   ENVOIE PAR MAIL   **
+	*************************
+*/
+// Email du destinataire
+$to = "maxim.berge@gmail.com";
+// Header du mail:
+$headers = 'From: '.$filter_result["email"]."\r\n".'Reply-To: '.$filter_result["email"]."\r\n"."Content-Type: text/html; charset=UTF-8";
+// Sujet du mail:
+$subject = "Nouveau message Hacker Poulette";
+// Message contenu dans le mail:
+$message_mail = "Nom: ".$filter_result["last_name"]."<br>"."Prénom: ".$filter_result["first_name"]."<br>"."Pays: ".$filter_result["country"]."<br>"."Sujet: ".$filter_result["subject"]."<br>"."Message: ".$filter_result["message"];
+// Fonction d'envoie du mail avec conf de ce que j'envoie
+mail($to, $subject, $message_mail, $headers);
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -39,19 +62,18 @@
 
     <body>
     <?php include "navbar.php" ?>
-       
+
         <h1>Formulaire de contact</h1>
         <div class="blue-divider"></div>
-        <!-- <div class="BOX"> -->
             <form class="container-fluid" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <section class="row line-form">
                     
                     <div class="col-lg-5 col-sm-12 txt-r">
-                        <label for="last_Name">Nom: </label> 
+                        <label  for="last_name">Nom: </label> 
                     </div>
 
                     <div class="col-lg-7 col-sm-12">
-                        <input class="name" name="last_Name" type="text">
+                        <input class="name" name="last_name" type="text">
                         <div class="test">
                             <label for="first_name">Prénom: </label>
                             <input class="name" name="first_name" type="text">
@@ -108,7 +130,6 @@
                 <input class="submit" type="submit" name="submit" value="envoyer">
             </section>
             </form>
-        <!-- </div> -->
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
