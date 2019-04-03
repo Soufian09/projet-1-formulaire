7950b3ee8c1a50cb28546f8cb21f0d03ff22c717
Soufian09 <46717440+Soufian09@users.noreply.github.com>
2019-04-03 15:54:38 +0200

    add fichier acceuil+produits

---
+++ acceuil.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hackers Roulette</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+ 
+</head>
+<body>                                                   
+      
+   
+   
+   <div class = "container-fluid">
+   <h2 class="title"> <u>Bienvenue chez Hackers Poulette</u> </h2>
+   <div class="carous">
+      <div id = "carouselwithIndicators" class = "carousel slide w-50" data-ride = "carousel">
+         <ol class = "carousel-indicators">
+            <li data-target = "#carouselExampleIndicators" data-slide-to = "0" class = "active"></li>
+            <li data-target = "#carouselExampleIndicators" data-slide-to = "1"></li>
+            <li data-target = "#carouselExampleIndicators" data-slide-to = "2"></li>
+         </ol>
+         
+         <div class = "carousel-inner">
+            <div class = "carousel-item active">
+            <a href="produits.html"> <img class = "d-block w-100" src ="img/raspi.gif" alt = "raspi.gif"></a> 
+               <div class = "carousel-caption d-none d-md-block">
+                  <h5>Découvrer les dernières nouveautés Raspberry!</h5>
+               </div>
+            </div>
+            
+            <div class = "carousel-item">
+            <a href="https://raspbian-france.fr/actualites-raspberry-pi/"> <img class = "d-block w-100" src = "img/news.gif" alt = "news.gif"></a>
+               <div class = "carousel-caption d-none d-md-block">
+                  <h5>Suivez l'actu tech pour ne rien rater </h5>
+               
+               </div>
+            </div>
+      
+            <div class = "carousel-item">
+            <a href="https://www.cdiscount.com/informatique/r-raspberry+pi+3.html#_his_">  <img class = "d-block w-100"  src = "img/promo.gif" alt = "promo.gif"> </a>
+              
+               <div class = "carousel-caption d-none d-md-block">
+                  <h5>PROMO SPECIAL !! </h5>
+               </div>
+            </div>
+            
+         </div>
+         
+         <a class = "carousel-control-prev" href = "#carouselwithIndicators" role = "button" data-slide = "prev">
+            <span class = "carousel-control-prev-icon" aria-hidden = "true"></span>
+            <span class = "sr-only">Précedent</span>
+         </a>
+         
+         <a class = "carousel-control-next" href = "#carouselwithIndicators" role =" button" data-slide = "next">
+            <span class = "carousel-control-next-icon" aria-hidden = "true"></span>
+            <span class = "sr-only">Suivant</span>
+         </a>
+      </div>
+   </div>
+</div>     
+
+   <script src = "https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+      integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+      crossorigin = "anonymous">
+   </script>
+   
+   <script src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+      integrity = "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
+      crossorigin = "anonymous">
+   </script>
+   
+   <script src = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+      integrity = "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+      crossorigin = "anonymous">
+   </script>
+
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+   <!-- Copyright -->
+   <div class="footer-copyright text-center py-3">© 2019 Copyright: Soufian / Maxim.
+      <a href="/acceuil.html"> hackers-roulette.com</a>
+   </div>
+</footer>
+    
+</body>
+</html>
---
+++ produits.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hackers Roulette</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+ 
+</head>
+<body>                                                   <!-- NAVBAR HACKERS -->
+
+<div class="container-fluid">
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="title"><u>Liste des produits</u></h1>
+    <!--LISTE DES PRODUITS-->
+    <div class="row one ">
+     
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp5.jpg" alt="rasp5"></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">Raspberry Pi 3 Model B, Quad Core CPU 1.2 GHz, 1 GB RAM</a>
+            </h4>
+            <h5>35.99€</h5>
+            <p class="card-text">
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p>
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp6.jpg" alt="rasp6"></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">Raspberry Pi Carte Mère Raspberry Pi 2 Type B (Processeur 900MHz, 1 Go de RAM, 4 x USB, 1 x HDMI, 1 x RJ45, 1 x Jack</a>
+            </h4>
+            <h5>40.99€</h5>
+            <p class="card-text">
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p>
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"> <span style="color: #00E5DE">★ ★ ★ ★ ☆</span></small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp7.jpg" alt=""></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">ABOX Raspberry Pi 3 B+ Starter Kit 16 Go SanDisk Micro SD Carte Classe 10, 5V 3A Alimentation Interrupteur Marche/Arrêt</a>
+            </h4>
+            <h5>24.99€</h5>
+            <p class="card-text">
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p>
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp8.jpg" alt=""></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">Raspberry Pi 3,14</a>
+            </h4>
+            <h5>24.99€</h5>
+            <p class="card-text">
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p> 
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp9.jpg" alt=""></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">Raspberry Pi 3 Official Desktop Starter Kit (16GB, WHite)</a>
+            </h4>
+            <h5>74.99€</h5>
+            <p class="card-text">
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p>
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ★ </span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="img/rasp8.jpg" alt=""></a>
+            <div class="card-body">
+            <h4 class="card-title">
+                <a href="#" style="color: #00E5DE">Raspberry Pi 3 Modèle B+, Carte-mère</a>
+            </h4>
+            <h5>34.99€</h5>
+            <p class="card-text"> 
+                <div class="text-muted">
+                    Marque :Melopero</p>
+                    Système d'exploitation :Linux</p>
+                    Ecran:	60 pouces</p>
+                    Connexions:	Wi-Fi</p>
+                    Nombre de ports USB	:4
+                    Batterie rechargeable:	Non</p>
+                </div>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ☆ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+            </div>
+        </div>
+        </div>
+
+    </div>
+    </div>
+</div>
+
+
+</div>
+        
+ <!-- Footer -->
+<footer class="page-footer font-small text-info">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright: Soufian / Maxim.
+      <a href="/acceuil.html"> hackers-roulette.com</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->                                           
+</body>
+</html>
