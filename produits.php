4312719f437fe32d0844c2b9caea7570790c7b3a
Soufian <soufianelhazman@gmail.com>
2019-04-05 14:24:09 +0200

    update contact (alt) + sémantique

--- produits.php
+++ produits.php
@@ -1,183 +1,171 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-    <title>Hackers Roulette</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta charset="utf-8">
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/produit.css">
-</head>
-
-<body>
-    <!-- NAVBAR HACKERS -->
-    <?php include "navbar.php" ?>
-    <h1>Liste des produits</h1>
-    <div class="blue-divider"></div>
-    <div class="container-fluid">
-        <div class="row">
+   <head>
+      <title>Hackers Roulette</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+      <meta charset="utf-8">
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="assets/css/produit.css">
+   </head>
+   <body>
+      <!-- NAVBAR HACKERS -->
+      <?php include "navbar.php" ?>
+      <h1>Liste des produits</h1>
+      <div class="blue-divider"></div>
+      <div class="container-fluid">
+         <div class="row">
             <div class="col-lg-12">
-                <!--LISTE DES PRODUITS-->
-                <div class="row one ">
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card h-100">
-                            <a href="#"><img class="card-img-top" src="assets/img/rasp5.jpg" alt="rasp5"></a>
-                            <div class="card-body">
-                                <h4 class="card-title">
-                                    <a href="#" style="color: #00E5DE">Raspberry Pi 3 Model B, Quad Core CPU 1.2 GHz, 1 GB RAM</a>
-                                </h4>
-                                <h5>35.99€</h5>
-                                <p class="card-text">
-                                    <div class="text-muted">
-                                        Marque :Melopero
-                                </p>
-                                Système d'exploitation :Linux</p>
-                                Ecran: 60 pouces</p>
-                                Connexions: Wi-Fi</p>
-                                Nombre de ports USB :4
-                                Batterie rechargeable: Non</p>
-                            </div>
+               <!--LISTE DES PRODUITS-->
+               <div class="row one ">
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp5.jpg" alt="Raspberry Pi 3 Model B, Quad Core CPU 1.2 GHz, 1 GB RAM"></a>
+                        <div class="card-body">
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">Raspberry Pi 3 Model B, Quad Core CPU 1.2 GHz, 1 GB RAM</a>
+                           </h4>
+                           <h5>35.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              Marque :Melopero
+                              </p>
+                              Système d'exploitation :Linux</p>
+                              Ecran: 60 pouces</p>
+                              Connexions: Wi-Fi</p>
+                              Nombre de ports USB :4
+                              Batterie rechargeable: Non</p>
+                           </div>
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                           <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
                         </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 mb-4">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top" src="assets/img/rasp6.jpg" alt="rasp6"></a>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp6.jpg" alt="Raspberry Pi Carte Mère Raspberry Pi 2 Type B"></a>
                         <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#" style="color: #00E5DE">Raspberry Pi Carte Mère Raspberry Pi 2 Type B (Processeur 900MHz, 1 Go de RAM, 4 x USB, 1 x HDMI, 1 x RJ45, 1 x Jack</a>
-                            </h4>
-                            <h5>40.99€</h5>
-                            <p class="card-text">
-                                <div class="text-muted">
-                                    Marque :Melopero
-                            </p>
-                            Système d'exploitation :Linux</p>
-                            Ecran: 60 pouces</p>
-                            Connexions: Wi-Fi</p>
-                            Nombre de ports USB :4
-                            Batterie rechargeable: Non</p>
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">Raspberry Pi Carte Mère Raspberry Pi 2 Type B (Processeur 900MHz, 1 Go de RAM, 4 x USB, 1 x HDMI, 1 x RJ45, 1 x Jack</a>
+                           </h4>
+                           <h5>40.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              Marque :Melopero
+                              </p>
+                              Système d'exploitation :Linux</p>
+                              Ecran: 60 pouces</p>
+                              Connexions: Wi-Fi</p>
+                              Nombre de ports USB :4
+                              Batterie rechargeable: Non</p>
+                           </div>
                         </div>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted"> <span style="color: #00E5DE">★ ★ ★ ★ ☆</span></small><button type="button" class="btn btn-info pull-right">Acheter</button>
-                    </div>
-                </div>
+                        <div class="card-footer">
+                           <small class="text-muted"> <span style="color: #00E5DE">★ ★ ★ ★ ☆</span></small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp7.jpg" alt="ABOX Raspberry Pi 3 B+ Starter Kit 16 Go SanDisk Micro SD "></a>
+                        <div class="card-body">
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">ABOX Raspberry Pi 3 B+ Starter Kit 16 Go SanDisk Micro SD Carte Classe 10, 5V 3A Alimentation Interrupteur Marche/Arrêt</a>
+                           </h4>
+                           <h5>24.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              Marque :Melopero
+                              </p>
+                              Système d'exploitation :Linux</p>
+                              Ecran: 60 pouces</p>
+                              Connexions: Wi-Fi</p>
+                              Nombre de ports USB :4
+                              Batterie rechargeable: Non</p>
+                           </div>
+                        </div>
+                        <div class="card-footer">
+                           <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp8.jpg" alt=""></a>
+                        <div class="card-body">
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">Raspberry Pi 3,14</a>
+                           </h4>
+                           <h5>24.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              <p> Marque :Melopero</p>
+                              <p>Système d'exploitation :Linux</p>
+                              <p>Ecran: 60 pouces</p>
+                              <p>Connexions: Wi-Fi</p>
+                              <p>Nombre de ports USB :4
+                              <p>Batterie rechargeable: Non</p>
+                           </div>
+                        </div>
+                        <div class="card-footer">
+                           <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp9.jpg" alt="Raspberry Pi 3 Official Desktop Starter Kit "></a>
+                        <div class="card-body">
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">Raspberry Pi 3 Official Desktop Starter Kit (16GB, WHite)</a>
+                           </h4>
+                           <h5>74.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              Marque :Melopero
+                              </p>
+                              Système d'exploitation :Linux</p>
+                              Ecran: 60 pouces</p>
+                              Connexions: Wi-Fi</p>
+                              Nombre de ports USB :4
+                              Batterie rechargeable: Non</p>
+                           </div>
+                        </div>
+                        <div class="card-footer">
+                           <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ★ </span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                     <div class="card h-100">
+                        <a href="#"><img class="card-img-top" src="assets/img/rasp8.jpg" alt="Raspberry Pi 3 Modèle B+"></a>
+                        <div class="card-body">
+                           <h4 class="card-title">
+                              <a href="#" style="color: #00E5DE">Raspberry Pi 3 Modèle B+, Carte-mère</a>
+                           </h4>
+                           <h5>34.99€</h5>
+                           <p class="card-text">
+                           <div class="text-muted">
+                              Marque :Melopero
+                              </p>
+                              Système d'exploitation :Linux</p>
+                              Ecran: 60 pouces</p>
+                              Connexions: Wi-Fi</p>
+                              Nombre de ports USB :4
+                              Batterie rechargeable: Non</p>
+                           </div>
+                        </div>
+                        <div class="card-footer">
+                           <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ☆ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
+                        </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-
-            <div class="col-lg-4 col-md-6 mb-4">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="assets/img/rasp7.jpg" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#" style="color: #00E5DE">ABOX Raspberry Pi 3 B+ Starter Kit 16 Go SanDisk Micro SD Carte Classe 10, 5V 3A Alimentation Interrupteur Marche/Arrêt</a>
-                        </h4>
-                        <h5>24.99€</h5>
-                        <p class="card-text">
-                            <div class="text-muted">
-                                Marque :Melopero
-                        </p>
-                        Système d'exploitation :Linux</p>
-                        Ecran: 60 pouces</p>
-                        Connexions: Wi-Fi</p>
-                        Nombre de ports USB :4
-                        Batterie rechargeable: Non</p>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-                <a href="#"><img class="card-img-top" src="assets/img/rasp8.jpg" alt=""></a>
-                <div class="card-body">
-                    <h4 class="card-title">
-                        <a href="#" style="color: #00E5DE">Raspberry Pi 3,14</a>
-                    </h4>
-                    <h5>24.99€</h5>
-                    <p class="card-text">
-                        <div class="text-muted">
-                            Marque :Melopero
-                    </p>
-                    Système d'exploitation :Linux</p>
-                    Ecran: 60 pouces</p>
-                    Connexions: Wi-Fi</p>
-                    Nombre de ports USB :4
-                    Batterie rechargeable: Non</p>
-                </div>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="assets/img/rasp9.jpg" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#" style="color: #00E5DE">Raspberry Pi 3 Official Desktop Starter Kit (16GB, WHite)</a>
-                </h4>
-                <h5>74.99€</h5>
-                <p class="card-text">
-                    <div class="text-muted">
-                        Marque :Melopero
-                </p>
-                Système d'exploitation :Linux</p>
-                Ecran: 60 pouces</p>
-                Connexions: Wi-Fi</p>
-                Nombre de ports USB :4
-                Batterie rechargeable: Non</p>
-            </div>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ★ ★ </span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
-        </div>
-    </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="assets/img/rasp8.jpg" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#" style="color: #00E5DE">Raspberry Pi 3 Modèle B+, Carte-mère</a>
-                </h4>
-                <h5>34.99€</h5>
-                <p class="card-text">
-                    <div class="text-muted">
-                        Marque :Melopero
-                </p>
-                Système d'exploitation :Linux</p>
-                Ecran: 60 pouces</p>
-                Connexions: Wi-Fi</p>
-                Nombre de ports USB :4
-                Batterie rechargeable: Non</p>
-            </div>
-        </div>
-        <div class="card-footer">
-            <small class="text-muted"><span style="color: #00E5DE">★ ★ ★ ☆ ☆</span> </small><button type="button" class="btn btn-info pull-right">Acheter</button>
-        </div>
-    </div>
-    </div>
-
-    </div>
-    </div>
-    </div>
-
-    </div>
-    <?php include "footer.php" ?>
-</body>
-
+         </div>
+      </div>
+      <?php include "footer.php" ?>
+   </body>
 </html>
